d958e80de0a85a211697aee65b44880ca9b5d357
roko-barbic <rokobarosk12345@gmail.com>
2023-06-07 15:44:38 +0200

    second commit

--- lv3_mobilne/lib/main.dart
+++ lv3_mobilne/lib/main.dart
@@ -1,19 +1,121 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        cupertinoOverrideTheme: const CupertinoThemeData(
+          brightness: Brightness.light,
         ),
+      ),
+      home: const MyHomePage(title: 'Popis svučilišta'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Future<List<dynamic>> data;
+
+  Future<List<dynamic>> fetchData() async {
+    final response = await http.get(
+        Uri.parse('http://universities.hipolabs.com/search?country=croatia'));
+
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body);
+      return jsonData;
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    data = fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CupertinoNavigationBar(
+        middle: Text(widget.title),
+      ),
+      body: FutureBuilder<List<dynamic>>(
+        future: data,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return const Center(
+              child: CupertinoAlertDialog(
+                title: Text("Error"),
+                content: Text(
+                    "URLSessionTask failed with error: The Internet connection appears to be offline."),
+                actions: [
+                  CupertinoDialogAction(
+                    child: Text("OK"),
+                  )
+                ],
+              ),
+            );
+          } else {
+            final data = snapshot.data!;
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                final item = data[index];
+                return Column(
+                  children: [
+                    ListTile(
+                      title: Text(item['name']),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(item['country']),
+                          Text(
+                            item['domains'].join(', '),
+                            style: const TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Divider(
+                      thickness: 1.5,
+                    ),
+                  ],
+                );
+              },
+            );
+          }
+        },
       ),
     );
   }
